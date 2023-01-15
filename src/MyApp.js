b19b3c152f4d383be029af1894b2417cdc215b6f
derussell23 <djamesr23@gmail.com>
2023-01-14 19:16:14 -0800

    Adding the form component after the table in MyApps.js

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,22 +1,24 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
-function MyApp() {
-  const [characters, setCharacters] = useState();
+function MyApp() 
+{
+    const [characters, setCharacters] = useState();
     
-function removeOneCharacter (index) {
-    const updated = characters.filter((character, i) => {
-        return i !== index
-        });
-        setCharacters(updated);
-    }
-      
+    function removeOneCharacter (index) {
+        const updated = characters.filter((character, i) => {
+            return i !== index
+            });
+            setCharacters(updated);
+        }
 
-return (
-    <div className="container">
-        <Table characterData={characters} removeCharacter={removeOneCharacter} />
-    </div>
-    )
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form />
+        </div>
+        )
 }
 
 export default MyApp;
